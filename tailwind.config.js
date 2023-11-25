ba1c05c7a1a1f5b67b5dbef442be8b1411eadd41
upa-r-upa <duekthtjf783@gmail.com>
2023-11-25 21:13:14 +0900

    init

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {},
+  plugins: [daisyui],
+};
