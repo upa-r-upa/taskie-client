20b80bf4926c7ea3d9473dab1f9dde5bec9bb188
upa-r-upa <me.uparupa@gmail.com>
2023-12-22 18:15:51 +0900

    feat: Change main layout to simple

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,9 @@ function App() {
     <div className="container max-w-xl mx-auto flex flex-col h-screen">
       <NavBar />
 
-      <div className="flex-1 overflow-y-auto">
-        <div className="max-w-lg flex items-center justify-center mx-auto">
-          <div className="min-w-full py-3 px-8">
-            <div className="font-sans">
-              <div className="container mx-auto max-w-md px-2 py-2">
-                <Outlet />
-              </div>
-            </div>
-          </div>
+      <div className="flex-1 w-full overflow-y-auto">
+        <div className="w-full min-h-full flex flex-col mx-auto max-w-md font-sans py-5 px-10">
+          <Outlet />
         </div>
       </div>
 
