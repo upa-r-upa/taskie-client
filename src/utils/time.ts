210b94d2f7b1841c8a40eaa4c29f6e8c3ee43a0f
upa-r-upa <me.uparupa@gmail.com>
2024-09-12 23:53:40 +0900

    refactor: Refactor date handling to use Day.js

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 export function getFormatMinutes(minutes: number): string {
   const hours = Math.floor(minutes / 60);
   const remainderMinutes = minutes % 60;
@@ -22,4 +24,17 @@ export function getFormatDayList(days: Array<number>): string {
   const result = days.map((day) => getFormatDay(day)).join(", ");
 
   return result;
+}
+
+export function formatDate(date: Date): string {
+  return dayjs(date).format("YYYY-MM-DD");
+}
+
+export function formatDateToUTC(date: Date): string {
+  return dayjs(date).utc().format("YYYY-MM-DD");
+}
+
+export function convertUtcToKst(utcDate: string): string {
+  const kstDate = dayjs.utc(utcDate).tz("Asia/Seoul");
+  return kstDate.format("YYYY-MM-DD HH:mm:ss");
 }
