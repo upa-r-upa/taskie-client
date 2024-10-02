55a54ce86c4a5b6dca484f9cd1006ab4530cc075
upa-r-upa <me.uparupa@gmail.com>
2024-10-03 01:18:50 +0900

    feat: enable date change functionality on the main page

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -6,11 +6,14 @@ import RoutineSection from "./RoutineSection";
 import { queryClient, taskApi } from "../../api/client";
 import Loading from "../../components/Loading";
 import { formatDate } from "../../utils/time";
+import Datepicker, { DateValueType } from "react-tailwindcss-datepicker";
+import { useState } from "react";
 
 function MainPage() {
+  const [targetDate, setTargetDate] = useState<Date>(new Date());
   const { isLoading, data } = useQuery({
-    queryKey: ["tasks"],
-    queryFn: () => taskApi.getAllDailyTask(formatDate(new Date())),
+    queryKey: ["tasks", formatDate(targetDate)],
+    queryFn: () => taskApi.getAllDailyTask(formatDate(targetDate)),
     refetchIntervalInBackground: true,
     refetchInterval: 60 * 1000,
   });
@@ -25,11 +28,32 @@ function MainPage() {
     });
   };
 
+  const handleValueChange = (value: DateValueType) => {
+    if (!value?.startDate) {
+      return;
+    }
+
+    setTargetDate(value.startDate);
+  };
+
   return (
     <>
-      <h1 className="text-2xl font-semibold mb-3">오늘 하루</h1>
+      <Datepicker
+        i18n="ko"
+        asSingle={true}
+        useRange={false}
+        displayFormat="YYYY년 MM월 DD일"
+        value={{
+          endDate: targetDate,
+          startDate: targetDate,
+        }}
+        placeholder="날짜를 지정해주세요."
+        popoverDirection="down"
+        onChange={handleValueChange}
+        inputClassName="input input-bordered w-full text-sm input-md "
+      />
 
-      <div className="container mb-5">
+      <div className="container my-5">
         <h2 className="text-xl mb-2">할 일</h2>
 
         <TodoSection
