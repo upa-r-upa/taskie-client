07204da57f0a5e115ae3d5d1a3691d607f55bd55
upa-r-upa <me.uparupa@gmail.com>
2024-11-11 21:59:41 +0900

    feat: Add PWA settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,56 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico"],
+      manifest: {
+        name: "Taskie: 투두/습관/루틴 관리",
+        short_name: "Taskie",
+        description: "간편하게 투두/습관/루틴을 관리하는 습관 관리 앱",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: [
       {
