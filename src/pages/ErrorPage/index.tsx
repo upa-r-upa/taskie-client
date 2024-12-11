7eeb347035771ddf2738a95275684c29b78ed89f
upa-r-upa <me.uparupa@gmail.com>
2024-12-11 20:22:39 +0900

    feat: Set vertical center to Error page

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -6,25 +6,34 @@ const ErrorPage = () => {
 
   if (error instanceof AxiosError) {
     return (
-      <div className="card card-compact">
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">문제가 발생했어요.</h2>
-          <div className="card-actions justify-end">{error.response?.data}</div>
+      <div className="h-screen flex items-center">
+        <div className="card card-compact m-auto">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">문제가 발생했어요.</h2>
+
+            <div className="card-actions justify-end">
+              {error.response?.data}
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 
   return (
-    <div className="card card-compact">
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">문제가 발생했어요!</h2>
-        <div>
-          <p>예상치 못한 문제가 발생했어요.</p>
-          <p>페이지를 새고로침 하거나, 나중에 다시 시도해주세요.</p>
-        </div>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">홈으로 돌아가기</button>
+    <div className="h-screen flex items-center">
+      <div className="card card-compact w-full">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">문제가 발생했어요!</h2>
+
+          <div>
+            <p>예상치 못한 문제가 발생했어요.</p>
+            <p>페이지를 새고로침 하거나, 나중에 다시 시도해주세요.</p>
+          </div>
+
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">홈으로 돌아가기</button>
+          </div>
         </div>
       </div>
     </div>
