45c8925ae87d2302b99047aab3729b7e955ef296
upa-r-upa <me.uparupa@gmail.com>
2025-01-16 01:11:15 +0900

    feat: Change sign-up page to shadcn

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -3,7 +3,7 @@ import UserNavigation from "./UserNavigation";
 
 export default function PageHeader() {
   return (
-    <header className="border-grid fixed top-0 z-50 w-full border-b">
+    <header className="border-grid fixed top-0 z-50 w-full border-b bg-background backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center px-4 mx-auto">
         <MainNavigation />
 
