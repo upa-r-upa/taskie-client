198422644b80c115942f0be6427c82b73c8ba9a0
upa-r-upa <me.uparupa@gmail.com>
2025-02-11 17:58:00 +0900

    feat: Change dayjs -> date-fns

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,10 @@ import React from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
 
 import "./global.css";
 import router from "./routes.tsx";
 import { queryClient } from "./api/client.ts";
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
