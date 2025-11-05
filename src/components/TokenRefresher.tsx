c9aa390bd906621842342428f0d0c80d59c23647
upa-r-upa <me.uparupa@gmail.com>
2025-11-05 17:34:40 +0900

    fix: fix linter

--- src/components/TokenRefresher.tsx
+++ src/components/TokenRefresher.tsx
@@ -42,7 +42,7 @@ export default function TokenRefresher() {
       .mutateAsync()
       .then((response) => {
         setToken(response.data!.access_token);
-        setUser(response.data!.user);
+        setUser(response.data!.user || null);
 
         client.defaults.headers.common["Authorization"] = authorizationToken(
           response.data!.access_token
@@ -90,7 +90,7 @@ export default function TokenRefresher() {
         const response = await refreshTokenMutation.mutateAsync();
 
         setToken(response.data!.access_token);
-        setUser(response.data!.user);
+        setUser(response.data!.user || null);
 
         client.defaults.headers.common["Authorization"] = authorizationToken(
           token!
